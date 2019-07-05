c2d1b47ccb5f4712e4fdd289df399dec7c94dba0
Ben Denger <ben@kreait.com>
2019-07-05 13:46:15 +0200

    mod

--- lib/common.sh
+++ lib/common.sh
@@ -89,7 +89,6 @@ install_jdk() {
   local install_dir=${1:?}
   local cache_dir=${2:?}
 
-  let start=$(nowms)
   JVM_COMMON_BUILDPACK=${JVM_COMMON_BUILDPACK:-https://buildpack-registry.s3.amazonaws.com/buildpacks/heroku/jvm.tgz}
   mkdir -p /tmp/jvm-common
   curl --retry 3 --silent --location $JVM_COMMON_BUILDPACK | tar xzm -C /tmp/jvm-common --strip-components=1
@@ -98,7 +97,6 @@ install_jdk() {
   source /tmp/jvm-common/opt/jdbc.sh
   mtime "jvm-common.install.time" "${start}"
 
-  let start=$(nowms)
   install_java_with_overlay "${install_dir}" "${cache_dir}"
   mtime "jvm.install.time" "${start}"
 }
